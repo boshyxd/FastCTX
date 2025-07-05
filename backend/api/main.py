d79b1b316339318c4e4827ad72717ceca835e7c4
Robert Babaev <github@robertbabaev.tech>
2025-07-05 16:06:12 -0400

    feat: start integrating an MCP

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
 
 app = FastAPI(title="FastCTX API")
 
@@ -6,3 +7,16 @@ app = FastAPI(title="FastCTX API")
 @app.post("/query")
 async def query_database():
     return {"Message:", "Hello, world"}
+
+
+@app.get("/schema")
+async def get_schema():
+    pass
+
+
+mcp = FastApiMCP(
+    app,
+    name="FastCTX API",
+    description="API for FastCTX - Get better context from your code.",
+)
+mcp.mount()
